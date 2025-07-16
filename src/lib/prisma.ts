ef93ace669ae43685551719c1831f2ef0799f3ce
Landon Garwood <landongarwood6@gmail.com>
2025-07-16 11:38:16 -0500

    feat: update prisma client

--- src/lib/prisma.ts
+++ src/lib/prisma.ts
@@ -4,6 +4,14 @@ const globalForPrisma = globalThis as unknown as {
   prisma: PrismaClient | undefined
 }
 
-export const prisma = globalForPrisma.prisma ?? new PrismaClient()
+const prismaClient = new PrismaClient({
+  datasources: {
+    db: {
+      url: process.env.DATABASE_URL + "?statement_cache=false",
+    },
+  },
+})
+
+export const prisma = globalForPrisma.prisma ?? prismaClient
 
 if (process.env.NODE_ENV !== "production") globalForPrisma.prisma = prisma
