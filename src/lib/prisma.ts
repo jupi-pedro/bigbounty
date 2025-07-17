21bdc30492d634d85253af50443d61153921d8a3
Landon Garwood <landongarwood6@gmail.com>
2025-07-17 14:51:35 -0500

    fix: update prisma client logic

--- src/lib/prisma.ts
+++ src/lib/prisma.ts
@@ -1,17 +1,19 @@
 import { PrismaClient } from "@prisma/client"
 
 const globalForPrisma = globalThis as unknown as {
-  prisma: PrismaClient | undefined
+  prisma?: PrismaClient
 }
 
-const prismaClient = new PrismaClient({
-  datasources: {
-    db: {
-      url: process.env.DATABASE_URL + "?statement_cache=false",
+const prisma =
+  globalForPrisma.prisma ??
+  new PrismaClient({
+    datasources: {
+      db: {
+        url: process.env.DATABASE_URL + "?statement_cache=false",
+      },
     },
-  },
-})
-
-export const prisma = globalForPrisma.prisma ?? prismaClient
+  })
 
 if (process.env.NODE_ENV !== "production") globalForPrisma.prisma = prisma
+
+export { prisma }
